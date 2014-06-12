5a384af01eaf19c3a1ab9e6f79315ad580827c7d
lekkala <lekkala.malakondaiah@jifflenow.com>
2014-06-12 14:34:58 +0530

    fourth commit

---
+++ views/song.rb
@@ -0,0 +1,13 @@
+require 'dm-core'
+require 'dm-migrations'
+require './song'
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+class Song
+  include DataMapper::Resource
+  property :id, Serial
+  property :title, String
+  property :lyrics, Text
+  property :length, Integer
+  property :released_on, Date
+end
+DataMapper.finalize
