68b555584f01031cd5c8978ef9770b9d516dda00
lekkala <lekkala.malakondaiah@jifflenow.com>
2014-06-12 14:38:04 +0530

    fifth commit

---
+++ song.rb
@@ -0,0 +1,23 @@
+require 'dm-core'
+require 'dm-migrations'
+
+configure do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+class Song
+  include DataMapper::Resource
+  property :id, Serial
+  property :title, String
+  property :lyrics, Text
+  property :length, Integer
+  property :released_on, Date
+end
+DataMapper.finalize
+get '/songs' do
+  @songs = Song.all
+  slim :songs
+end
+get '/songs/:id' do
+  @song = Song.get(params[:id])
+  slim :show_song
+end
